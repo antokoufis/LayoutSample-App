df415aefac2ce8d3c3394a824300432e863889e4
antokoufis <antokoufis@gmail.com>
2022-04-04 19:44:26 +0300

    Create a new Activity

---
+++ app/src/main/java/com/example/layoutsample/LayoutSample.java
@@ -0,0 +1,14 @@
+package com.example.layoutsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LayoutSample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_sample);
+    }
+}
